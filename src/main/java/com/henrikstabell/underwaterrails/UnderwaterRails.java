8df9155f45d313ee4005031db921564d4ae987ec
Ole Henrik Stabell <ole@henrikstabell.com>
2018-03-30 19:36:48 +0200

    Initial commit

---
+++ src/main/java/com/henrikstabell/underwaterrails/UnderwaterRails.java
@@ -0,0 +1,35 @@
+package com.henrikstabell.underwaterrails;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import org.apache.logging.log4j.Logger;
+
+/**
+ * See The repos LICENSE.MD file for what you can and can't do with the code.
+ * Created by Hennamann(Ole Henrik Stabell) on 30/03/2018.
+ */
+@Mod(modid = UnderwaterRails.MODID, name = UnderwaterRails.NAME, version = UnderwaterRails.VERSION)
+public class UnderwaterRails
+{
+    public static final String MODID = "underwaterrails";
+    public static final String NAME = "Underwater Rails";
+    public static final String VERSION = "1.0.0";
+
+    public static Logger logger;
+
+    @Mod.EventHandler
+    public void preInit(FMLPreInitializationEvent event) {
+        logger = event.getModLog();
+    }
+
+    @Mod.EventHandler
+    public void init(FMLInitializationEvent event) {
+    }
+
+    @Mod.EventHandler
+    public void postInit(FMLPostInitializationEvent event) {
+
+    }
+}
