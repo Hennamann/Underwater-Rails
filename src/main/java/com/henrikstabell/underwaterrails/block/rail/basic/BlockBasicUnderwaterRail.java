8e7ab530ac3640611230c79087e024513346ed84
Ole Henrik Stabell <ole@henrikstabell.com>
2022-01-02 20:20:20 +0100

    Final 1.16 cleanup

--- src/main/java/com/henrikstabell/underwaterrails/block/rail/basic/BlockBasicUnderwaterRail.java
+++ src/main/java/com/henrikstabell/underwaterrails/block/rail/basic/BlockBasicUnderwaterRail.java
@@ -35,145 +35,14 @@ public class BlockBasicUnderwaterRail extends RailBlock implements IWaterLoggabl
         this.registerDefaultState(this.stateDefinition.any().setValue(SHAPE, RailShape.NORTH_SOUTH).setValue(WATERLOGGED, Boolean.valueOf(false)));
     }
 
-    @Override
-    protected void updateState(BlockState blockState, World world, BlockPos blockPos, Block block) {
-        if (block.defaultBlockState().isSignalSource() && (new RailState(world, blockPos, blockState)).countPotentialConnections() == 3) {
-            this.updateDir(world, blockPos, blockState, false);
-        }
-    }
-
-    @Override
     public Property<RailShape> getShapeProperty() {
         return SHAPE;
     }
 
-    @Override
-    public BlockState rotate(BlockState blockState, Rotation rotation) {
-        switch(rotation) {
-            case CLOCKWISE_180:
-                switch((RailShape)blockState.getValue(SHAPE)) {
-                    case ASCENDING_EAST:
-                        return blockState.setValue(SHAPE, RailShape.ASCENDING_WEST);
-                    case ASCENDING_WEST:
-                        return blockState.setValue(SHAPE, RailShape.ASCENDING_EAST);
-                    case ASCENDING_NORTH:
-                        return blockState.setValue(SHAPE, RailShape.ASCENDING_SOUTH);
-                    case ASCENDING_SOUTH:
-                        return blockState.setValue(SHAPE, RailShape.ASCENDING_NORTH);
-                    case SOUTH_EAST:
-                        return blockState.setValue(SHAPE, RailShape.NORTH_WEST);
-                    case SOUTH_WEST:
-                        return blockState.setValue(SHAPE, RailShape.NORTH_EAST);
-                    case NORTH_WEST:
-                        return blockState.setValue(SHAPE, RailShape.SOUTH_EAST);
-                    case NORTH_EAST:
-                        return blockState.setValue(SHAPE, RailShape.SOUTH_WEST);
-                    case NORTH_SOUTH: //Forge fix: MC-196102
-                    case EAST_WEST:
-                        return blockState;
-                }
-            case COUNTERCLOCKWISE_90:
-                switch((RailShape)blockState.getValue(SHAPE)) {
-                    case ASCENDING_EAST:
-                        return blockState.setValue(SHAPE, RailShape.ASCENDING_NORTH);
-                    case ASCENDING_WEST:
-                        return blockState.setValue(SHAPE, RailShape.ASCENDING_SOUTH);
-                    case ASCENDING_NORTH:
-                        return blockState.setValue(SHAPE, RailShape.ASCENDING_WEST);
-                    case ASCENDING_SOUTH:
-                        return blockState.setValue(SHAPE, RailShape.ASCENDING_EAST);
-                    case SOUTH_EAST:
-                        return blockState.setValue(SHAPE, RailShape.NORTH_EAST);
-                    case SOUTH_WEST:
-                        return blockState.setValue(SHAPE, RailShape.SOUTH_EAST);
-                    case NORTH_WEST:
-                        return blockState.setValue(SHAPE, RailShape.SOUTH_WEST);
-                    case NORTH_EAST:
-                        return blockState.setValue(SHAPE, RailShape.NORTH_WEST);
-                    case NORTH_SOUTH:
-                        return blockState.setValue(SHAPE, RailShape.EAST_WEST);
-                    case EAST_WEST:
-                        return blockState.setValue(SHAPE, RailShape.NORTH_SOUTH);
-                }
-            case CLOCKWISE_90:
-                switch((RailShape)blockState.getValue(SHAPE)) {
-                    case ASCENDING_EAST:
-                        return blockState.setValue(SHAPE, RailShape.ASCENDING_SOUTH);
-                    case ASCENDING_WEST:
-                        return blockState.setValue(SHAPE, RailShape.ASCENDING_NORTH);
-                    case ASCENDING_NORTH:
-                        return blockState.setValue(SHAPE, RailShape.ASCENDING_EAST);
-                    case ASCENDING_SOUTH:
-                        return blockState.setValue(SHAPE, RailShape.ASCENDING_WEST);
-                    case SOUTH_EAST:
-                        return blockState.setValue(SHAPE, RailShape.SOUTH_WEST);
-                    case SOUTH_WEST:
-                        return blockState.setValue(SHAPE, RailShape.NORTH_WEST);
-                    case NORTH_WEST:
-                        return blockState.setValue(SHAPE, RailShape.NORTH_EAST);
-                    case NORTH_EAST:
-                        return blockState.setValue(SHAPE, RailShape.SOUTH_EAST);
-                    case NORTH_SOUTH:
-                        return blockState.setValue(SHAPE, RailShape.EAST_WEST);
-                    case EAST_WEST:
-                        return blockState.setValue(SHAPE, RailShape.NORTH_SOUTH);
-                }
-            default:
-                return blockState;
-        }
-    }
-
-    @Override
-    public BlockState mirror(BlockState blockState, Mirror mirror) {
-        RailShape railshape = blockState.getValue(SHAPE);
-        switch(mirror) {
-            case LEFT_RIGHT:
-                switch(railshape) {
-                    case ASCENDING_NORTH:
-                        return blockState.setValue(SHAPE, RailShape.ASCENDING_SOUTH);
-                    case ASCENDING_SOUTH:
-                        return blockState.setValue(SHAPE, RailShape.ASCENDING_NORTH);
-                    case SOUTH_EAST:
-                        return blockState.setValue(SHAPE, RailShape.NORTH_EAST);
-                    case SOUTH_WEST:
-                        return blockState.setValue(SHAPE, RailShape.NORTH_WEST);
-                    case NORTH_WEST:
-                        return blockState.setValue(SHAPE, RailShape.SOUTH_WEST);
-                    case NORTH_EAST:
-                        return blockState.setValue(SHAPE, RailShape.SOUTH_EAST);
-                    default:
-                        return super.mirror(blockState, mirror);
-                }
-            case FRONT_BACK:
-                switch(railshape) {
-                    case ASCENDING_EAST:
-                        return blockState.setValue(SHAPE, RailShape.ASCENDING_WEST);
-                    case ASCENDING_WEST:
-                        return blockState.setValue(SHAPE, RailShape.ASCENDING_EAST);
-                    case ASCENDING_NORTH:
-                    case ASCENDING_SOUTH:
-                    default:
-                        break;
-                    case SOUTH_EAST:
-                        return blockState.setValue(SHAPE, RailShape.SOUTH_WEST);
-                    case SOUTH_WEST:
-                        return blockState.setValue(SHAPE, RailShape.SOUTH_EAST);
-                    case NORTH_WEST:
-                        return blockState.setValue(SHAPE, RailShape.NORTH_EAST);
-                    case NORTH_EAST:
-                        return blockState.setValue(SHAPE, RailShape.NORTH_WEST);
-                }
-        }
-
-        return super.mirror(blockState, mirror);
-    }
-
-    @Override
     public FluidState getFluidState(BlockState state) {
         return state.getValue(WATERLOGGED) ? Fluids.WATER.getSource(false) : super.getFluidState(state);
     }
 
-    @Override
     protected void createBlockStateDefinition(StateContainer.Builder<Block, BlockState> builder) {
         builder.add(SHAPE, WATERLOGGED);
     }
--- src/main/java/com/henrikstabell/underwaterrails/handler/EventHandler.java
+++ src/main/java/com/henrikstabell/underwaterrails/handler/EventHandler.java
@@ -18,29 +18,36 @@ public class EventHandler {
 
     @SubscribeEvent
     public static void onMinecartDrownDamage(LivingHurtEvent event) {
-        if (event.getSource() == DamageSource.DROWN && event.getEntityLiving().getRootVehicle() instanceof MinecartEntity && event.getEntityLiving().getCommandSenderWorld().getBlockState(((MinecartEntity) event.getEntityLiving().getRootVehicle()).getCurrentRailPosition()).getBlock() instanceof IUnderwaterRail && ((IUnderwaterRail) event.getEntityLiving().getCommandSenderWorld().getBlockState(((MinecartEntity) event.getEntityLiving().getRootVehicle()).getCurrentRailPosition()).getBlock()).waterBreathingWhenOnRail()) {
-            event.getEntity().setAirSupply(300);
-            event.setCanceled(true);
-        }
+        if (event.getSource() == DamageSource.DROWN)
+            if (event.getEntityLiving().getRootVehicle() instanceof MinecartEntity)
+                if (event.getEntityLiving().getCommandSenderWorld().getBlockState(((MinecartEntity) event.getEntityLiving().getRootVehicle()).getCurrentRailPosition()).getBlock() instanceof IUnderwaterRail)
+                    if (((IUnderwaterRail) event.getEntityLiving().getCommandSenderWorld().getBlockState(((MinecartEntity) event.getEntityLiving().getRootVehicle()).getCurrentRailPosition()).getBlock()).waterBreathingWhenOnRail()) {
+                        event.getEntity().setAirSupply(300);
+                        event.setCanceled(true);
+                    }
     }
     @OnlyIn(Dist.CLIENT)
     @SubscribeEvent
     public static void onRenderFogUnderwater(EntityViewRenderEvent.FogDensity event) {
         if (event.getInfo().getFluidInCamera().is(FluidTags.WATER))
-            if (event.getInfo().getEntity().getRootVehicle() instanceof MinecartEntity && event.getInfo().getEntity().getCommandSenderWorld().getBlockState(((MinecartEntity) event.getInfo().getEntity().getRootVehicle()).getCurrentRailPosition()).getBlock() instanceof IUnderwaterRail && ((IUnderwaterRail) event.getInfo().getEntity().getCommandSenderWorld().getBlockState(((MinecartEntity) event.getInfo().getEntity().getRootVehicle()).getCurrentRailPosition()).getBlock()).waterBreathingWhenOnRail()) {
-                GL11.glFogf(GL11.GL_FOG_DENSITY, 0.00025f);
-                event.setCanceled(true);
-            }
+            if (event.getInfo().getEntity().getRootVehicle() instanceof MinecartEntity)
+                if (event.getInfo().getEntity().getCommandSenderWorld().getBlockState(((MinecartEntity) event.getInfo().getEntity().getRootVehicle()).getCurrentRailPosition()).getBlock() instanceof IUnderwaterRail)
+                    if (((IUnderwaterRail) event.getInfo().getEntity().getCommandSenderWorld().getBlockState(((MinecartEntity) event.getInfo().getEntity().getRootVehicle()).getCurrentRailPosition()).getBlock()).waterBreathingWhenOnRail()) {
+                        GL11.glFogf(GL11.GL_FOG_DENSITY, 0.00025f);
+                        event.setCanceled(true);
+                    }
     }
 
     @OnlyIn(Dist.CLIENT)
     @SubscribeEvent
     public static void onRenderFogColorUnderwater(EntityViewRenderEvent.FogColors event) {
         if (event.getInfo().getFluidInCamera().is(FluidTags.WATER))
-            if (event.getInfo().getEntity().getRootVehicle() instanceof MinecartEntity && event.getInfo().getEntity().getCommandSenderWorld().getBlockState(((MinecartEntity) event.getInfo().getEntity().getRootVehicle()).getCurrentRailPosition()).getBlock() instanceof IUnderwaterRail && ((IUnderwaterRail) event.getInfo().getEntity().getCommandSenderWorld().getBlockState(((MinecartEntity) event.getInfo().getEntity().getRootVehicle()).getCurrentRailPosition()).getBlock()).waterBreathingWhenOnRail()) {
-                event.setBlue(0);
-                event.setRed(0);
-                event.setGreen(0);
-            }
+            if (event.getInfo().getEntity().getRootVehicle() instanceof MinecartEntity)
+                if (event.getInfo().getEntity().getCommandSenderWorld().getBlockState(((MinecartEntity) event.getInfo().getEntity().getRootVehicle()).getCurrentRailPosition()).getBlock() instanceof IUnderwaterRail)
+                    if (((IUnderwaterRail) event.getInfo().getEntity().getCommandSenderWorld().getBlockState(((MinecartEntity) event.getInfo().getEntity().getRootVehicle()).getCurrentRailPosition()).getBlock()).waterBreathingWhenOnRail()) {
+                        event.setBlue(0);
+                        event.setRed(0);
+                        event.setGreen(0);
+                    }
     }
 }
